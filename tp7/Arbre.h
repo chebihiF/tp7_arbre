0f02949d90c614603fd5d7da4471bd3538f6b26b
chebihiF <chebihi.f@ucd.ac.ma>
2022-05-23 16:32:51 +0100

    ajouter, parcourir et rechercher dans une arbre binaire

--- tp7/Arbre.h
+++ tp7/Arbre.h
@@ -13,27 +13,62 @@ typedef Sheet* Tree;
 Sheet* createSheet(int);
 Tree createTree(int, Sheet*, Sheet*);
 void addValue(int, Tree*);
+void addValueR(int, Tree*);
+void recherch(int, Tree);
 
-
-void addValue(int value, Tree* arbre) {
+void recherch(int val, Tree arbre) {
 	if (arbre == NULL)
-		*arbre = (Tree) malloc(sizeof(Sheet));
-	else {
-		Tree p = *arbre;
-		while (p!=NULL)
-		{
-			if (value > p->value)
-				p = p->right;
-			else
-				p = p->left;
-		}
-		p = (Tree)malloc(sizeof(Sheet));
-		p->value = value;
-		p->left = NULL;
-		p->right = NULL;
+		printf("valeur introuvable");
+	else
+	{
+		if (val == arbre->value)
+			printf("La valeur est : %d",val);
+		else if (val > arbre->value)
+			recherch(val, arbre->right);
+		else
+			recherch(val, arbre->left);
 	}
 }
 
+
+void addValueR(int value, Tree* arbre) {
+	if (*arbre == NULL) {
+		(*arbre) = (Tree)malloc(sizeof(Sheet));
+		(*arbre)->value = value;
+		(*arbre)->left = NULL;
+		(*arbre)->right = NULL;
+	}
+	else
+	{
+		if ((*arbre)->value > value)
+			addValueR(value, &((*arbre)->left));
+		else
+			addValueR(value, &((*arbre)->right));
+	}
+}
+
+void addValue(int value, Tree* arbre) {
+	if (*arbre == NULL) {
+		(*arbre) = (Tree)malloc(sizeof(Sheet));
+		(*arbre)->value = value;
+		(*arbre)->left = NULL;
+		(*arbre)->right = NULL;
+	}
+	else {
+		Tree* p = arbre;
+		while (*p != NULL)
+		{
+			if (value > (*p)->value)
+				p = &((*p)->right);
+			else
+				p = &((*p)->left);
+		}
+		*p = (Tree)malloc(sizeof(Sheet));
+		(*p)->value = value;
+		(*p)->left = NULL;
+		(*p)->right = NULL;
+	}
+}
 
 Sheet* createSheet(int value) {
 	Sheet* s1 = (Sheet*) malloc(sizeof(Sheet));
--- tp7/main.c
+++ tp7/main.c
@@ -1,6 +1,19 @@
 #include "Arbre.h";
 
 void main() {
+
+	Tree t = NULL;
+
+	addValueR(8, &t);
+	addValueR(5, &t);
+	addValueR(16, &t);
+	addValueR(2, &t);
+	addValueR(12, &t);
+	addValueR(3, &t);
+	addValueR(18, &t);
+
+	recherch(4,t);
+
 	/*
 	Tree arbre = createTree(
 		0
@@ -8,7 +21,7 @@ void main() {
 		, createTree(1, createSheet(4), createSheet(8))
 	);*/
 
-	Sheet* f1 = createSheet(6);
+	/*Sheet* f1 = createSheet(6);
 	Sheet* f2 = createSheet(5);
 	Tree r1 = createTree(3, f1, f2);
 
@@ -16,7 +29,9 @@ void main() {
 	Sheet* f4 = createSheet(8);
 	Tree r2 = createTree(1, f3, f4);
 
-	Tree arbre = createTree(0, r1, r2);
+	Tree arbre = createTree(0, r1, r2);*/
+
+
 
 
 }
